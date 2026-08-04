ea21bb9eccc6e749c0fe03e1f516fdedf741bf68
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-04 17:15:50 +0300

    ProductService Interface Added

---
+++ Services/IProductService.cs
@@ -0,0 +1,13 @@
+using productCatalogAPI.Models;
+
+namespace productCatalogAPI.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product? GetById(int id);
+        Product? Create(Product product);
+        bool Update(int id,Product product);
+        bool Delete(int id);
+    }
+}
