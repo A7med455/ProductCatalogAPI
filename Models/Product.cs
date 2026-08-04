926f5b7a25634c6ffa23207e6e474c3110aea7cf
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-04 17:14:32 +0300

    Product Model Class Updated with namespace

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,7 +1,10 @@
-public class Product
+namespace productCatalogAPI.Models
 {
-    public int Id{get; set; }
-    public string Name{get; set; }
-    public string? Description{get; set; }
-    public double Price{get; set; }
+    public class Product
+    {
+        public int Id{get; set; }
+        public string Name{get; set; }
+        public string? Description{get; set; }
+        public double Price{get; set; }
+    }
 }
