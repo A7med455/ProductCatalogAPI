ca242abcc720d56759ddfb7ddece20c44df6480d
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-04 17:58:33 +0300

    Updates

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using productCatalogAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Register controllers so ASP.NET Core scans for classes like ProductsController
+builder.Services.AddControllers();
+
+// Register the service: whenever something asks for IProductService, give it a ProductService
+builder.Services.AddScoped<IProductService, ProductService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Wires up all your [HttpGet], [HttpPost], etc. routes from your controllers
+app.MapControllers();
 
 app.Run();
--- obj/Debug/net10.0/productCatalogAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/productCatalogAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("productCatalogAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b108fa8f758e93a707150075adf1b2069956b39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ffd57512e682dc5e7a43e6c5e32faa0960bb14")]
 [assembly: System.Reflection.AssemblyProductAttribute("productCatalogAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("productCatalogAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
