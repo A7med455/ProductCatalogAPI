a8ffd57512e682dc5e7a43e6c5e32faa0960bb14
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-04 17:16:14 +0300

    ProductService Added

---
+++ Services/ProductService.cs
@@ -0,0 +1,48 @@
+using productCatalogAPI.Models;
+namespace productCatalogAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private List<Product> products = new()
+        {
+            new Product { Id = 1, Name = "Laptop", Description = "Gaming laptop", Price = 25000 },
+            new Product { Id = 2, Name = "Mouse", Description = "Wireless mouse", Price = 300 }
+        };
+
+        public List<Product> GetAll()
+        {
+            return products;    
+        }
+        //FirstOrDefault scans the list and returns the first product whose Id matches, or null if none found
+        public Product? GetById(int id)=>products.FirstOrDefault(p => p.Id == id);
+        public Product? Create(Product product)
+        {
+            product.Id = products.Count > 0 ? products.Max(p => p.Id) + 1 : 1;
+            products.Add(product);
+            return product;
+        }
+        public bool Update(int id,Product UpdatedProduct)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if(product == null)
+            {
+                return false;
+            }
+            product.Name = UpdatedProduct.Name;
+            product.Description = UpdatedProduct.Description;
+            product.Price = UpdatedProduct.Price;
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id== id);
+            if(product == null)
+            {
+                return false;
+            }
+            products.Remove(product);
+            return true;
+        }
+
+    }
+}
