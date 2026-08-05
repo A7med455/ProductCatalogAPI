650f6bf02033a3b1c315d53ee71db34fa3338743
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-08-06 02:45:37 +0300

    More wrong inputs expected are handled

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -3,6 +3,7 @@ namespace productCatalogAPI.Services
 {
     public class ProductService : IProductService
     {
+        private const double MaxPrice = 1000000;
         private List<Product> products = new()
         {
             new Product { Id = 1, Name = "Laptop", Description = "Gaming laptop", Price = 25000 },
@@ -17,9 +18,13 @@ namespace productCatalogAPI.Services
         public Product? GetById(int id)=>products.FirstOrDefault(p => p.Id == id);
         public Product? Create(Product product)
         {
-            if(product.Price < 0)
+            if(product.Price < 0||product.Price > MaxPrice)
             {
-                throw new ArgumentException("Price cannot be negative.");
+                throw new ArgumentException("Price cannot be negative or exceed pricing limits.");
+            }
+            if(string.IsNullOrEmpty(product.Name))
+            {
+                throw new ArgumentException("Product Name is Empty or null");
             }
             product.Id = products.Count > 0 ? products.Max(p => p.Id) + 1 : 1;
             products.Add(product);
@@ -27,14 +32,22 @@ namespace productCatalogAPI.Services
         }
         public bool Update(int id,Product UpdatedProduct)
         {
+            if(id < 0)
+            {
+                throw new ArgumentException("id cannot be negative.");
+            }
             var product = products.FirstOrDefault(p => p.Id == id);
             if(product == null)
             {
                 return false;
             }
-            if(UpdatedProduct.Price < 0)
+            if(UpdatedProduct.Price < 0 ||UpdatedProduct.Price > MaxPrice)
             {
-                throw new ArgumentException("Price cannot be negative.");
+                throw new ArgumentException("Price cannot be negative or exceed pricing limits.");
+            }
+            if(string.IsNullOrEmpty(UpdatedProduct.Name))
+            {
+                throw new ArgumentException("Product Name is Empty or null");
             }
             product.Name = UpdatedProduct.Name;
             product.Description = UpdatedProduct.Description;
@@ -43,6 +56,10 @@ namespace productCatalogAPI.Services
         }
         public bool Delete(int id)
         {
+            if(id < 0)
+            {
+                throw new ArgumentException("id cannot be negative.");
+            }
             var product = products.FirstOrDefault(p => p.Id== id);
             if(product == null)
             {
